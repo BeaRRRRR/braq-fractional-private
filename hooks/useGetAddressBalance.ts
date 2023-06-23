ac87df5935ef5dcc43d6c9b0adf105130f2a9080
BeaRRRRR <mikhail.kedzel@gmail.com>
2023-06-23 21:27:36 +0900

    fix address

--- hooks/useGetAddressBalance.ts
+++ hooks/useGetAddressBalance.ts
@@ -4,7 +4,7 @@ export default function useGetAddressBalance() {
   const [balance, setBalance] = useState(null);
 
   async function fetchBalance() {
-    const resp = await fetch(`https://api.etherscan.io/api?module=account&action=balance&address=${process.env.NEXT_PUBLIC_ETHERSCAN_ADDRESS}&tag=latest&apikey=${process.env.NEXT_PUBLIC_ETHERSCAN_API_KEY}`);
+    const resp = await fetch(`https://api.etherscan.io/api?module=account&action=balance&address=${process.env.NEXT_PUBLIC_CONTRACT_ADDRESS}&tag=latest&apikey=${process.env.NEXT_PUBLIC_ETHERSCAN_API_KEY}`);
     const json = await resp.json();
     setBalance(json.result);
   }
